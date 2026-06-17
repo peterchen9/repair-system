79325e8d327f37906424cfb34f4d3d69036a69ad
peterchen9 <207313468+peterchen9@users.noreply.github.com>
2026-06-17 18:41:59 +0800

    修復既有追蹤碼時間格式轉換

---
+++ prisma/migrations/20260617001000_fix_timestamp_tracking_codes/migration.sql
@@ -0,0 +1,8 @@
+UPDATE "RepairItem"
+SET "trackingCode" = strftime(
+  '%Y%m%d%H%M%S',
+  datetime(replace(substr("createdAt", 1, 19), 'T', ' '), '+' || "id" || ' seconds', '+8 hours')
+)
+WHERE "trackingCode" IS NULL
+   OR length("trackingCode") != 14
+   OR "trackingCode" GLOB '*[^0-9]*';
